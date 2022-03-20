e53546d61c0e2875ebfaf50eff689372308df0a7
tylerjusfly <tylerjusfly1@gmail.com>
2022-03-20 16:42:03 +0100

    begining

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -32,6 +32,46 @@ exports.productController = {
     }
   }, //end of create
 
+  getAll : async(req, res, next) => {
+    const alls = await Product.find().populate('shop', 'shopName')
+    res.status(200).send({
+      message : "Getting all Products",
+      count : alls.length,
+      products : alls.map( all => {
+        return {
+          name : all.name,
+          shop : all.shop,
+          _id : all._id,
+          request : {
+            type : 'GET',
+            url : `localhost:3000/shop/${all.shop._id}/products/${all._id}`
+          }
+        }
+      })
+    });
+  },
+
+  getById : async(req, res, next) => {
+    const id = req.params.productId;
+    const product = await Product.findById(id)
+    if(!product){  
+      res.status(400).json({message : "No valid id entry for the provided id."})
+    }
+
+    try {
+      res.status(200).json({
+        message : "Get Product by Id",
+        product : product
+      })
+    } catch (err) {
+      res.status(500).json({
+        error : err
+      })
+    };
+      
+    }
+    
+
 
 
 
--- api/routes/products.js
+++ api/routes/products.js
@@ -1,54 +1,15 @@
 const express = require('express');
 const router = express.Router({mergeParams : true})
 const Product = require('../models/products');
-const {verifyToken} = require('../controllers/verifyToken');
+const {verifyToken, verifyTokenAndAdmin} = require('../controllers/verifyToken');
 const {productController} = require('../controllers/products')
 
 
 router.post('/', verifyToken, productController.create);
 
-router.get('/', async(req, res, next) => {
-  const alls = await Product.find()
-  res.status(200).send({
-    message : "Getting all Products",
-    count : alls.length,
-    products : alls.map( all => {
-      return {
-        name : all.name,
-        price : all.price,
-        _id : all._id,
-        request : {
-          type : 'GET',
-          url : `localhost:3000/products/${all._id}`
-        }
-      }
-    })
-  });
-});
+router.get('/', verifyTokenAndAdmin, productController.getAll);
 
-router.get('/:productId', (req, res, next) => {
-  const id = req.params.productId;
-  Product.findById(id)
-  .exec()
-  .then(data => {
-    if (data){
-      res.status(200).json({
-        message : "product by Id",
-        data : data
-      });
-    }
-    else{
-      res.status(400).json({message : "No valid id entry for the provided id."})
-    }
-  })
-  .catch(err => {
-    console.log(err );
-    res.status(500).json({
-      error : err
-    })
-  })
-
-});
+router.get('/:productId', verifyToken, productController.getById);
 
 router.patch('/:productId', async(req, res, next) => {
   try{
